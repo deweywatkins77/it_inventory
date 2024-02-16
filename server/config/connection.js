d6ebeeb11cf75f4f3682dd52a1bc584e42a79dc7
Dewey Watkins <deweywatkins77@gmail.com>
2024-02-16 08:33:29 -0500

    db connect update

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,10 +2,10 @@
 const { connect, connection } = require('mongoose');
 
 // Explicitly set the URI (replace with your actual MongoDB URI)
-const uri = 'mongodb://root:root@localhost:27017/invManagement?authMechanism=DEFAULT&authSource=admin';
+const uri = 'mongodb+srv://root:Timrp123!@cluster0.wprmk4t.mongodb.net/invManagement?retryWrites=true&w=majority&authMechanism=DEFAULT';
 console.log('URI:', uri);
 
-connect('mongodb://root:root@localhost:27017/invManagement?authMechanism=DEFAULT&authSource=admin', {
+connect('mongodb+srv://root:Timrp123!@cluster0.wprmk4t.mongodb.net/invManagement?retryWrites=true&w=majority&authMechanism=DEFAULT', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -7,12 +7,13 @@ const devices = require('./devices.json');
 const qrcodes = require('./qrcodes.json')
 
 db.once('open', async () => {
-  await Devices.deleteMany({})
-  await QRCodes.deleteMany({})
+  const deleteDev = await Devices.deleteMany({})
+  const deleteQR = await QRCodes.deleteMany({})
 
-  await Devices.create(devices)
-  await QRCodes.create(qrcodes)
+  const createDev = await Devices.create(devices)
+  const createQR = await QRCodes.create(qrcodes)
 
+  console.log(deleteDev, deleteQR, createDev, createQR)
   console.log('all done!')
   process.exit(0)
 });
